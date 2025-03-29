1dc7bdf3049fb3d9c0a64e0f718e98a366aae5a3
Shamla Abdul Hameed <shamlaabdulhameed@IN-C02F57JUMD6M.local>
2025-03-29 15:01:12 +0400

    minor changes for url

--- scraper_project/scraper_app/urls.py
+++ scraper_project/scraper_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import search_articles
 
 urlpatterns = [
-    path("api/search/", search_articles, name="search_articles"),
+    path("news/search/", search_articles, name="search_articles"),
 ]
